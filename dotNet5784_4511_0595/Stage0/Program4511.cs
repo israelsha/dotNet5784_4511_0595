6c4798448705e88d8845160e34d0f302545e1479
Israel_sha <054304hayim@gmail.com>
2023-12-27 21:23:00 +0200

    Initial Code + Welcome functions

---
+++ dotNet5784_4511_0595/Stage0/Program4511.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome4511();
+        Welcome0595();
+
+    }
+    static partial void Welcome0595();
+    private static void Welcome4511()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.Write(name);
+        Console.Write(", welcome to my first console application");
+    }
+}
