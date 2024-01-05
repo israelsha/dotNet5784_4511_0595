1430e0ae65e6fe66beb3a1ab23ccc41b9655af08
eliya masa <eliyamasa@gmail.com>
2024-01-05 12:37:06 +0200

    Chapter 3 and 4

---
+++ dotNet5784_4511_0595/DalFacade/DO/Dependency.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System.Reflection.Emit;
+using System.Xml.Linq;
+
+namespace DO;
+
+public record Dependency
+{
+   int Id;
+   int? DependentTask = null;
+    int? DependsOnTask = null;
+    public Dependency() { Id = 0;}
+
+    public Dependency(int id, int? dependentTask, int? dependsOnTask)
+    {
+        Id = id;
+        DependentTask = dependentTask;
+        DependsOnTask = dependsOnTask;
+    }
+}
+
+
+
---
+++ dotNet5784_4511_0595/DalFacade/DO/Engineer.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Xml;
+
+namespace DO;
+
+public record Engineer
+{
+    int Id;
+    string Email;
+    double Cost;  //daily cost of the engineer, including salary, workplace, tools
+    string Name;
+    DO.EngineerExperience Level;
+
+    public Engineer() { Id = 0; Email = ""; Cost = 0.0 ; Name = ""; Level = EngineerExperience.Beginner; }
+
+    public Engineer(int id, string email, double cost, string name)
+    {
+        Id = id;
+        Email = email;
+        Cost = cost;
+        Name = name;
+    }
+}
