f816b288ce4e3143eaf353aac7c8b8800258bea6
eliya masa <eliyamasa@gmail.com>
2024-01-28 12:42:06 +0200

    stage 4 chapter 4- 6.3

---
+++ BL/BO/Enums.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Diagnostics.Contracts;
+
+namespace BO;
+
+enum Status
+{
+  Unscheduled,
+  Scheduled,
+  OnTrack,
+  InJeopardy,
+  Done
+}
+
+enum EngineerExperience {
+  Beginner,
+  AdvancedBeginner,
+  Intermediate,
+  Advanced,
+  Expert
+}
