e66ab94ae3270c4b12c61fcadc4df688cb6b88ae
Israel_sha <054304hayim@gmail.com>
2024-01-31 09:47:19 +0200

    begining of chapter 8

---
+++ BL/BlImplementation/MilestoneImplementation.cs
@@ -0,0 +1,16 @@
+﻿using BlApi;
+
+namespace BlImplementation;
+
+internal class MilestoneImplementation : IMilestone
+{
+    public BO.Milestone? Create(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(BO.Milestone item)
+    {
+        throw new NotImplementedException();
+    }
+}
