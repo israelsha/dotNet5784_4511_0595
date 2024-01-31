fec46bf133f18a5aa81038413034e591a964640f
Israel_sha <054304hayim@gmail.com>
2024-01-31 23:30:27 +0200

    once more Never-ending 8

--- BL/BO/Tools.cs
+++ BL/BO/Tools.cs
@@ -1,4 +1,7 @@
 ﻿namespace BO;
+using BlImplementation;
+using DalApi;
+using Dal;
 using System.Net.Mail;
 internal class Tools
 {
@@ -15,4 +18,11 @@ internal class Tools
         }
     }
 
+    public static DO.Task? findTask(int ingineerId)
+    {
+        IEnumerable<DO.Task> tasks = DalApi.Factory.Get.Task.ReadAll();//bring the list of all tasks
+        return (from item in tasks     //select the task that this (our) engineer is doing (ie: id=EngineerId)
+                where (item.EngineerId == ingineerId)
+                select item).FirstOrDefault();
+    }
 }
